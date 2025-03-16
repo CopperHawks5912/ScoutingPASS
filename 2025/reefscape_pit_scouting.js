9f72a3a6e5aa28e68e40f0c3105f51125eaba3ba
MorkXII <101903260+MorkXII@users.noreply.github.com>
2025-03-16 17:19:42 -0400

    Update reefscape_pit_scouting.js

--- 2025/reefscape_pit_scouting.js
+++ 2025/reefscape_pit_scouting.js
@@ -9,24 +9,12 @@ var config_data = `
       "code": "t",
       "type": "number"
     },
-    { "name": "Width",
-      "code": "wid",
-      "type": "number",
-      "defaultValue": "0"
-    },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": "0"
-    },
     { "name": "Drivetrain",
       "code": "drv",
       "type": "radio",
       "choices": {
         "s": "Swerve<br>",
-        "w": "West Coast/Tank<br>",
-        "b": "Butterfly/Grashopper<br>",
-        "m": "Mechanum<br>",
+        "t": "Tank<br>",
         "o": "Other"
       },
       "defaultValue": "o"
@@ -37,53 +25,60 @@ var config_data = `
       "size": 20,
       "maxSize": 50
     },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "Drivetrain Motor",
-      "code": "mot",
-      "type": "radio",
-      "choices": {
-        "n": "Neo<br>",
-        "f": "Falcon<br>",
-        "c": "CIM<br>",
-        "x": "Other<br>"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "# of Batteries",
-      "code": "nob",
-      "type": "number"
-    },
     { "name": "Floor pickup Coral",
-      "code": "fpu",
+      "code": "fpc",
       "type": "bool"
     },
-    { "name": "Floor pickup Algae",
-      "code": "fpa",
+    { "name": "Score Coral - L1",
+      "code": "sc1",
+      "type": "bool"
+    },
+    { "name": "Score Coral - L2",
+      "code": "sc2",
+      "type": "bool"
+    },
+    { "name": "Score Coral - L3",
+      "code": "sc3",
+      "type": "bool"
+    },
+    { "name": "Score Coral - L4",
+      "code": "sc4",
+      "type": "bool"
+    },
+    { "name": "Score Algae - Processor",
+      "code": "sap",
+      "type": "bool"
+    },
+    { "name": "Score Algae - Net",
+      "code": "san",
       "type": "bool"
     },
     { "name": "Autos",
       "code": "aut",
+      "type": "radio",
+      "choices": {
+        "m": "Move Only<br>",
+        "n": "One Coral<br>",
+        "u": "Multi Coral<br>",
+        "o": "Other"
+      },
+      "defaultValue": "o"
+    },
+    { "name": "Auto Notes",
+      "code": "atn",
       "type": "text",
       "size": 20,
       "maxSize": 250
     },
-    { "name": "Scouting Method /<br>Program (ScoutingPASS?)",
-      "code": "sct",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
+    { "name": "Climb",
+      "code": "cli",
+      "type": "radio",
+      "choices": {
+        "d": "Deep Climp<br>",
+        "s": "Shallow Climb<br>",
+        "n": "None"
+      },
+      "defaultValue": "o"
     },
     { "name": "Comments",
       "code": "co",

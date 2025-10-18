61ea1f2d83bfb8a43bc5d05c15e7d79e7b8670c6
MorkXII <101903260+MorkXII@users.noreply.github.com>
2025-10-18 07:16:52 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025oncmp1",
+      "defaultValue": "2025onwat",
       "required": "true"
     },
     { "name": "Match Level",

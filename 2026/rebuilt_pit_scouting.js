33c3c57be3fa6ae526556532ab78b8d7bce13692
MorkXII <101903260+MorkXII@users.noreply.github.com>
2026-03-13 19:56:03 -0400

    Update rebuilt_pit_scouting.js

--- 2026/rebuilt_pit_scouting.js
+++ 2026/rebuilt_pit_scouting.js
@@ -9,24 +9,12 @@ var config_data = `
       "code": "t",
       "type": "number"
     },
-    { "name": "Width",
-      "code": "wid",
-      "type": "number",
-      "defaultValue": "0"
-    },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": "0"
-    },
     { "name": "Drivetrain",
       "code": "drv",
       "type": "radio",
       "choices": {
         "s": "Swerve<br>",
-        "w": "West Coast/Tank<br>",
-        "b": "Butterfly/Grashopper<br>",
-        "m": "Mechanum<br>",
+        "w": "Tank<br>",
         "o": "Other"
       },
       "defaultValue": "o"
@@ -37,54 +25,12 @@ var config_data = `
       "size": 20,
       "maxSize": 50
     },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "Drivetrain Motor",
-      "code": "mot",
-      "type": "radio",
-      "choices": {
-        "n": "Neo<br>",
-        "f": "Falcon<br>",
-        "c": "CIM<br>",
-        "x": "Other<br>"
-      },
-      "defaultValue":"x"
-    },
-    { "name": "# of Batteries",
-      "code": "nob",
-      "type": "number"
-    },
-    { "name": "Floor pickup Fuel",
-      "code": "fpu",
-      "type": "bool"
-    },
-    { "name": "Depot pickup Fuel",
-      "code": "dpu",
-      "type": "bool"
-    },
-    { "name": "Outpost pickup Fuel",
+    { "name": "Auto Climb",
       "code": "opu",
       "type": "bool"
     },
-    { "name": "Autos",
+    { "name": "Auto Notes",
       "code": "aut",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Scouting Method /<br>Program (ScoutingPASS?)",
-      "code": "sct",
       "type": "text",
       "size": 20,
       "maxSize": 250
